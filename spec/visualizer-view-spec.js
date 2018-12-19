22957cf20069d23ce26db6f585d0fab6c8b18f66
lika85456 <lika85456@post.cz>
2018-12-19 11:49:23 +0100

    first

---
+++ spec/visualizer-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import VisualizerView from '../lib/visualizer-view';
+
+describe('VisualizerView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
